1bce58834ab6ffe230bcc100a0491a5b1ce84714
gadgeski <xtwh.ce5bil@gmail.com>
2025-09-19 12:10:50 +0900

    test: (RealEstateStarterTests/RealEstateStarterTests.swift)

---
+++ RealEstateStarterTests/RealEstateStarterTests.swift
@@ -0,0 +1,16 @@
+//
+//  RealEstateStarterTests.swift
+//  RealEstateStarterTests
+//
+//  Created by Dev Tech on 2025/09/18.
+//
+
+import Testing
+
+struct RealEstateStarterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
